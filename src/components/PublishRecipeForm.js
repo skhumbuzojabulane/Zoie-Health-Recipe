defa93c9c7e0c4c3570f8d2fcd42fcc4624f5475
Skhumbuzojabulane <skhumbuzojabulane@gmail.com>
2023-07-01 00:04:06 +0200

    30 June 2023

---
+++ src/components/PublishRecipeForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PublishRecipeForm() {
+    return (
+        <h1>Welcome to the form</h1>
+    );
+}
+
+export default PublishRecipeForm; 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-
 import '../styles/dashboard.css';
+
+import PublishRecipeForm from '../components/PublishRecipeForm';
 
 function Dashboard({ setIsDashboardScreen, isDashboardScreen }) {
   const location = useLocation();
+  const [content, setContent] = useState('');
+  const [selectedMenuItem, setSelectedMenuItem] = useState('publish');
+  const [buttonClicked, setButtonClicked] = useState(false);
 
   useEffect(() => {
     setIsDashboardScreen(location.pathname === '/dashboard');
@@ -16,6 +20,19 @@ function Dashboard({ setIsDashboardScreen, isDashboardScreen }) {
     console.log('Logged out');
   };
 
+  const handleMenuItemClick = (menuItem) => {
+    setSelectedMenuItem(menuItem);
+
+    // Set the content based on the clicked menu item
+    if (menuItem === 'publish') {
+      setContent(<PublishRecipeForm />);
+    } else if (menuItem === 'all') {
+      setContent('This is the All Recipes content.');
+    } else if (menuItem === 'about') {
+      setContent('This is the About App content.');
+    }
+  };
+
   return (
     <div className={`dashboard-container ${isDashboardScreen ? 'hide-header' : ''}`}>
       <div className="control-panel">
@@ -24,15 +41,19 @@ function Dashboard({ setIsDashboardScreen, isDashboardScreen }) {
           </div>
           <div className='menuBackground'>
             <ul className="menu">
-            <li>Publish Recipe</li>
-            <li>All recipes</li>
-            <li>About App</li>
-        </ul>
+              <li onClick={() => handleMenuItemClick('publish')}>Publish Recipe</li>
+              <li onClick={() => handleMenuItemClick('all')}>All Recipes</li>
+              <li onClick={() => handleMenuItemClick('about')}>About App</li>
+            </ul>
           </div>
       </div>
       <div className="content-area">
+        {content}
         {/* Render content based on the selected menu item */}
       </div>
+      {buttonClicked && (
+        <div className="indicator">Button Clicked!</div>
+      )}
     </div>
   );
 }
